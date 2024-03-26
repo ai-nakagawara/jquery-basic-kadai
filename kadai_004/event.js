74e1bb6fc01fbb96156d0bc5f971ab485b4f689f
ai-nakagawara <163296333+ai-nakagawara@users.noreply.github.com>
2024-03-26 17:38:36 +0900

    kadai_004の提出

---
+++ kadai_004/event.js
@@ -0,0 +1,9 @@
+$(window).on('load', function () {
+    // ロード完了したとき
+    console.log('loadイベントが発生しました');
+});
+
+$(window).on('scroll', () => {
+    // スクロールしたとき
+    console.log('scrollイベントが発生しました');
+});
